a8391a278de3da8c0096465f3358152753a768fe
jarod.davis37 <jarod.davis37@ku.edu>
2019-09-30 16:37:00 -0500

    Adding Exercise1 and an index.html home page

---
+++ home/html/jdscript.js
@@ -0,0 +1,35 @@
+function message()
+{
+    document.getElementById("message").style.display = "inline-block";
+}
+
+function checkPass()
+{
+    var password = document.getElementById('password2');
+    var confirm = document.getElementById('confirm2');
+    var message = document.getElementById('confirm-message2');
+    var good_color = "#66cc66";
+    var bad_color = "#ff6666";
+
+    if (password.value == confirm.value)
+    {
+      if(password.value.length >= 8)
+      {
+        confirm.style.backgroundColor = good_color;
+        message.style.color = good_color;
+        message.innerHTML = "Passwords Match!";
+      }
+      else
+      {
+        confirm.style.backgroundColor = bad_color;
+        message.style.color = bad_color;
+        message.innerHTML = "Password must contain at least 8 characters.";
+      }
+    }
+    else
+    {
+        confirm.style.backgroundColor = bad_color;
+        message.style.color = bad_color;
+        message.innerHTML = "Passwords do not match. Please re-enter your password.";
+    }
+}
